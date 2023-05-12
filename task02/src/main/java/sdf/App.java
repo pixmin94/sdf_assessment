7e9279013422bdeeaabfe66f7e2475f8b4819643
pixmin94 <pixmin@outlook.com>
2023-05-12 14:59:53 +0800

    get all files in subdirectories

--- task02/src/main/java/sdf/App.java
+++ task02/src/main/java/sdf/App.java
@@ -1,48 +1,78 @@
 package sdf;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.Files;
+
+import java.io.IOException;
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+
+import java.util.stream.Stream;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-
-import java.io.IOException;
 
 public class App 
 {
     public static void main( String[] args ) throws IOException
     {
+        String directory = args[0];
+        List<Path> pathList = new ArrayList<>();
 
-        FileReader fr = new FileReader(args[0]);
-        BufferedReader br = new BufferedReader(fr);
-  
-        ArrayList<String> wordsList = new ArrayList<>();
-        br.lines()
-            .map(line -> line.trim().toLowerCase().replaceAll("\\p{Punct}", ""))
-            .map(line -> line.split("\\s+"))
-            .flatMap(w -> Arrays.asList(w).stream())
-            .forEach(w -> wordsList.add(w))
-            ;
-        String[] wordsArray = wordsList.toArray(new String[0]);
-        // System.out.println(wordsArray);
-
-        
-        Map<String, ArrayList<String>> wordPairs = new HashMap<String, ArrayList<String>>(); 
-
-        for (int i = 0; i < wordsArray.length-1; i++) {
-            wordPairs.computeIfAbsent(wordsArray[i], k -> new ArrayList<>()).add(wordsArray[i+1]);
+        try (Stream<Path> stream = Files.walk(Paths.get(directory))) {
+        pathList = stream.map(Path::normalize)
+            .filter(Files::isRegularFile)
+            .skip(1)
+            .collect(Collectors.toList());
         }
-        System.out.println(wordPairs);
+        pathList.forEach(System.out::println);
 
-        // Map<String, Map<String, Value> finalMap = new HashMap<String, HashMap<String,Value>>();
+        for (Path path : pathList) {
+            File filename = new File(path.toString());
+            FileReader fr = new FileReader(filename);
+            BufferedReader br = new BufferedReader(fr);
+
+            ArrayList<String> wordsList = new ArrayList<>();
+            br.lines()
+                .map(line -> line.trim().toLowerCase().replaceAll("\\p{Punct}", ""))
+                .map(line -> line.split("\\s+"))
+                .flatMap(w -> Arrays.asList(w).stream())
+                .forEach(w -> wordsList.add(w))
+                ;
+            String[] wordsArray = wordsList.toArray(new String[0]);
+            // System.out.println(wordsArray);
+            
+            Map<String, ArrayList<String>> wordPairs = new HashMap<String, ArrayList<String>>(); 
+
+            for (int i = 0; i < wordsArray.length-1; i++) {
+                wordPairs.computeIfAbsent(wordsArray[i], k -> new ArrayList<>()).add(wordsArray[i+1]);
+            }
+            // System.out.println(wordPairs);
+
+            // Map<String, Map<String, Value> finalMap = new HashMap<String, HashMap<String,Value>>();
+            for (Map.Entry<String, ArrayList<String>> set : wordPairs.entrySet()) {
+                Map<String, Integer> innerMap = new HashMap<>();
+                for (String s : set.getValue()) {
+                    if (innerMap.containsKey(s))
+                        // change to probability instead of count
+                        innerMap.put(s, innerMap.get(s) + 1);
+                    else
+                        innerMap.put(s, 1);
+                }
+                System.out.println(set.getKey());
+                System.out.println("   " + innerMap);
+            }
+
+        }
 
     }
+
 }
